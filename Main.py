79249be0eb0aae84a3c7c5809e614aedcaacae68
Stefano Carobene <stefano.carobene04@gmail.com>
2023-08-12 10:10:54 +0200

    inizializzazione varie funzioni main

---
+++ Main.py
@@ -0,0 +1,25 @@
+# due funzioni una di scraping e una di ricerca link nel sito
+# la funzione di scraping prende in input un link e restituisce una lista di link
+# la funzione di ricerca naviga il sito e copia tutti i link in una lista
+
+from bs4 import BeautifulSoup as bs
+import requests
+import re
+import os
+
+
+def salvataggio(caratteristiche):
+    return 0
+
+def scraping(link):
+    return 0
+
+def ricerca():
+    return 0
+
+def main():
+    ricerca()
+    input("desideri aggiungere un link alla lista preferiti? (y/n)")
+    if input == "y":
+        link = input("inserisci il link: ")
+        scraping(link)
