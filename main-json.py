d71d848650ee90b5d121f9a3e69834cc91b9d61d
Stefano Carobene <stefano.carobene04@gmail.com>
2023-08-16 09:25:47 +0200

    implementazione try-except

--- main-json.py
+++ main-json.py
@@ -6,12 +6,17 @@ from bs4 import BeautifulSoup as bs
 import requests
 import os
 import json
+from tenacity import retry, stop_after_attempt, wait_fixed
+import sys 
+
 
 
 # Creazione file csv se non esiste
 if not os.path.isfile('yacht.json'):
     with open('yacht.json', 'w', encoding='utf-8') as f:
         f.write('[]')
+        
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(5))  # Retry 3 times with a 5-second delay between retries
 
 def salvataggio(caratteristiche):
     # Load existing data
@@ -36,146 +41,169 @@ def salvataggio(caratteristiche):
     return 0
 
 def scraping(link):
-    page = requests.get(link)
-    soup = bs(page.content, 'html.parser')
-    caratteristiche = {}  # Change caratteristiche from a list to a dictionary
+    try:
+        page = requests.get(link)
+        soup = bs(page.content, 'html.parser')
+        caratteristiche = {}  # Change caratteristiche from a list to a dictionary
 
-    caratteristiche["link"] = link
-    type_of_boat_element = soup.find('a', {'title': 'Power'}) or soup.find('a', {'title': 'Sail'})
-    type_of_boat = type_of_boat_element.get_text() if type_of_boat_element else None
-    caratteristiche["type m/s"] = type_of_boat
+        caratteristiche["link"] = link
+        type_of_boat_element = soup.find('a', {'title': 'Power'}) or soup.find('a', {'title': 'Sail'})
+        type_of_boat = type_of_boat_element.get_text() if type_of_boat_element else None
+        caratteristiche["type m/s"] = type_of_boat
 
-    # Check if the element was found before accessing attributes
-    titolo_element = soup.find('h1', class_='heading')
-    caratteristiche["titolo"] = titolo_element.text if titolo_element else None
+        # Check if the element was found before accessing attributes
+        titolo_element = soup.find('h1', class_='heading')
+        caratteristiche["titolo"] = titolo_element.text if titolo_element else None
 
-    descrizione_element = soup.find('div', class_='description')
-    caratteristiche["descrizione"] = descrizione_element.text if descrizione_element else None
+        descrizione_element = soup.find('div', class_='description')
+        caratteristiche["descrizione"] = descrizione_element.text if descrizione_element else None
 
-    cash_element = soup.find('span', class_='payment-total')
-    caratteristiche["cash"] = cash_element.text if cash_element else None
+        cash_element = soup.find('span', class_='payment-total')
+        caratteristiche["cash"] = cash_element.text if cash_element else None
 
-    location_element = soup.find('h2', class_='location')
-    caratteristiche["location"] = location_element.text if location_element else None
+        location_element = soup.find('h2', class_='location')
+        caratteristiche["location"] = location_element.text if location_element else None
 
-    details_container = soup.find("div", class_="collapse-content-details open")
+        details_container = soup.find("div", class_="collapse-content-details open")
 
-    if details_container:
-        details_table = details_container.find("table", class_="datatable-section")
-        
-        if details_table:
-            data_points = details_table.find_all("tr", class_="datatable-item")
-        
-            # Loop through data points and extract key-value pairs
-            scraped_data = {}
-            for data_point in data_points:
-                title = data_point.find("td", class_="datatable-title").text
-                value = data_point.find("td", class_="datatable-value").text
-                scraped_data[title] = value
-            caratteristiche["scraped_data"] = scraped_data
+        if details_container:
+            details_table = details_container.find("table", class_="datatable-section")
+            
+            if details_table:
+                data_points = details_table.find_all("tr", class_="datatable-item")
+            
+                # Loop through data points and extract key-value pairs
+                scraped_data = {}
+                for data_point in data_points:
+                    title = data_point.find("td", class_="datatable-title").text
+                    value = data_point.find("td", class_="datatable-value").text
+                    scraped_data[title] = value
+                caratteristiche["scraped_data"] = scraped_data
+            else:
+                print("Details table not found.")
         else:
-            print("Details table not found.")
-    else:
-        print("Details container not found.")
+            print("Details container not found.")
+
+            
+        propulsion_container = soup.find("div", class_="detail-data-table propulsion")
+        if propulsion_container:
+            engine_categories = propulsion_container.find_all("div", class_="datatable-category")
+
+            engine_details = []
+            for engine_category in engine_categories:
+                engine_data = {}
+                engine_name = engine_category.find("h3", class_="sub-title").text.strip()
+                engine_data["Engine Name"] = engine_name
+
+                details_table = engine_category.find("table", class_="datatable-section")
+                det = details_table.find_all("tr", class_="datatable-item")
+                for data_point in det:
+                    title = data_point.find("td", class_="datatable-title").text
+                    value = data_point.find("td", class_="datatable-value").text
+                    engine_data[title] = value
+
+                engine_details.append(engine_data)
+            
+            caratteristiche["engine_details"] = engine_details
+        else:
+            print("No engine details found")
+
 
         
-    propulsion_container = soup.find("div", class_="detail-data-table propulsion")
-    if propulsion_container:
-        engine_categories = propulsion_container.find_all("div", class_="datatable-category")
+        specs_container = soup.find("div", class_="detail-data-table measurements")
+        if specs_container:
+            specs_categories = specs_container.find_all("div", class_="datatable-category")
 
-        engine_details = []
-        for engine_category in engine_categories:
-            engine_data = {}
-            engine_name = engine_category.find("h3", class_="sub-title").text.strip()
-            engine_data["Engine Name"] = engine_name
+            specs_details = []
+            for specs_category in specs_categories:
+                specs_data = {}
+                specs_name = specs_category.find("h3", class_="sub-title").text.strip()
+                specs_data["specs"] = specs_name
 
-            details_table = engine_category.find("table", class_="datatable-section")
-            det = details_table.find_all("tr", class_="datatable-item")
-            for data_point in det:
-                title = data_point.find("td", class_="datatable-title").text
-                value = data_point.find("td", class_="datatable-value").text
-                engine_data[title] = value
+                details_table = specs_category.find("table", class_="datatable-section")
+                det = details_table.find_all("tr", class_="datatable-item")
 
-            engine_details.append(engine_data)
+                for data_point in det:
+                    title = data_point.find("td", class_="datatable-title").text
+                    value = data_point.find("td", class_="datatable-value").text
+                    specs_data[title] = value
+
+                specs_details.append(specs_data)
+            
+            caratteristiche["specs_details"] = specs_details
+        else:
+            print("No specs details found")
         
-        caratteristiche["engine_details"] = engine_details
-    else:
-        print("No engine details found")
-
+        print(caratteristiche)
+        salvataggio(caratteristiche)  # Pass the dictionary directly
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        raise e  # Re-raise the exception to trigger the retry mechanism
 
     
-    specs_container = soup.find("div", class_="detail-data-table measurements")
-    if specs_container:
-        specs_categories = specs_container.find_all("div", class_="datatable-category")
-
-        specs_details = []
-        for specs_category in specs_categories:
-            specs_data = {}
-            specs_name = specs_category.find("h3", class_="sub-title").text.strip()
-            specs_data["specs"] = specs_name
-
-            details_table = specs_category.find("table", class_="datatable-section")
-            det = details_table.find_all("tr", class_="datatable-item")
-
-            for data_point in det:
-                title = data_point.find("td", class_="datatable-title").text
-                value = data_point.find("td", class_="datatable-value").text
-                specs_data[title] = value
-
-            specs_details.append(specs_data)
-        
-        caratteristiche["specs_details"] = specs_details
-    else:
-        print("No specs details found")
-    
-    print(caratteristiche)
-    salvataggio(caratteristiche)  # Pass the dictionary directly
     return 0
 
 def ricerca():
-    max = 100
-    min = 1
+    min = 139000
+    max = 140000
     while True:
+        jk = 0
         k = 0
         x = 1
         while True:
-            link = f'https://www.yachtworld.com/boats-for-sale/price-{min},{max}/page-{x}/'
-            print(link)
-            lista = []
-            lista_vecchia = lista
-            r = requests.get(link)
-            soup = bs(r.text, 'html.parser')
-            lista = soup.find_all('a', href=True)
-            lista = [i for i in lista if i['href'].startswith('/yacht/')]
-            if lista == lista_vecchia:
-                break
-            if len(lista) == 0:
-                break
-            else:
-                for i in lista:
-                    print(i['href'])
-                    # check if the link is already present
-                    link_art = 'https://www.yachtworld.com' + i['href']
-                    with open('yacht.json', 'r', encoding='utf-8') as f:
-                        existing_data = json.load(f)
-                    existing_links = [entry['link'] for entry in existing_data]
-                    if link_art not in existing_links:
-                        print(link)
-                        scraping(link_art)
-                        k = 0
-                    else:
-                        k += 1
-                        print('link già presenteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-                        
-                        print(k)
-                        if k > 500:
-                            for _ in range(50):
-                                print('link già presenteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+            try:
+                link = f'https://www.yachtworld.com/boats-for-sale/price-{min},{max}/page-{x}/'
+                print(link)
+                lista = []
+                lista_vecchia = lista
+                r = requests.get(link)
+                soup = bs(r.text, 'html.parser')
+                lista = soup.find_all('a', href=True)
+                lista = [i for i in lista if i['href'].startswith('/yacht/')]
+                if lista == lista_vecchia:
+                    break
+                if len(lista) == 0:
+                    break
+                else:
+                    len_lista = len(lista) 
+                    jk = 0
+                    for i in lista:
+                        print(i['href'])
+                        # check if the link is already present
+                        link_art = 'https://www.yachtworld.com' + i['href']
+                        with open('yacht.json', 'r', encoding='utf-8') as f:
+                            existing_data = json.load(f)
+                        existing_links = [entry['link'] for entry in existing_data]
+                        if link_art not in existing_links:
+                            print(link)
+                            scraping(link_art)
+                            jk = 0
+                            k = 0
+                        else:
+                            jk += 1
+                            if jk == len_lista:
+                                k += 15
+                                jk = 0
+                            print(f'len_lista = {len_lista},                     jk = {jk},                  k = {k}')
+                            print(f'')
+
+                            print('link già presenteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+                            if k > 500:
+                                for _ in range(50):
+                                    print('presenteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
                                 x = 250
+                                jk = 0 
+
+            except requests.exceptions.RequestException as e:
+                print("Error:", e)
+                raise e  # Re-raise the exception to trigger the retry mechanism
+
+            
             x += 1
-        min += 100
-        max += 100
-    return 0
+        if max == 1550005000:
+            sys.exit()
+        min += 1000
+        max += 1000
 
 def main():
     ricerca()
